bbf50a2c8596e1ea7334dea99b1d447d5286bcc7
victormc13 <viktorsb14@gmail.com>
2023-06-28 19:49:23 -0500

    part2 exercise completed

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Coche miCoche = new Coche();
         miCoche.agregarPuertas();
-        miCoche.agregarPuertas();
         System.out.println(miCoche.puertas);
     }
 }
