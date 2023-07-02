6f1df6896e6ba604fee91117e31d4d4c721a0cf6
victormc13 <viktorsb14@gmail.com>
2023-07-02 01:39:29 -0500

    Add class Person with private variable age

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,31 @@
 public class Main {
 
     public static void main(String[] args) {
-    Person me = new Person();
+    Person person = new Person();
 
-    me.setName("Victor");
+    person.setName("Victor");
+    person.setAge(23);
 
-    System.out.println("My name is:  " + me.getName());
+    System.out.println("My name is " + person.getName());
+    System.out.println("I'm " + person.getAge() + " years old");
     }
 }
 
 class Person {
     private String name;
+    private int age;
 
     public void setName(String name){
         this.name = name;
     }
     public String getName() {
         return name;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+    public int getAge() {
+        return age;
     }
 }
