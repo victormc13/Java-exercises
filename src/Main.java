f6305d73367897e16f7d5d8ff051a16c276adc2e
victormc13 <viktorsb14@gmail.com>
2023-07-02 01:47:44 -0500

    Add theme8 exercise completed

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,15 @@
 public class Main {
 
     public static void main(String[] args) {
-    Person person = new Person();
+        Person person = new Person();
 
-    person.setName("Victor");
-    person.setAge(23);
-    person.setPhone(931928801);
+        person.setName("Victor");
+        person.setAge(23);
+        person.setPhone(931928801);
 
-    System.out.println("My name is " + person.getName());
-    System.out.println("I'm " + person.getAge() + " years old");
-    System.out.println("My phone is " + person.getPhone());
+        System.out.println("My name is " + person.getName());
+        System.out.println("I'm " + person.getAge() + " years old");
+        System.out.println("My phone is " + person.getPhone());
     }
 }
 
@@ -20,23 +20,27 @@ class Person {
     private int age;
     private long phone;
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
+
     public String getName() {
         return name;
     }
 
-    public void setAge(int age){
+    public void setAge(int age) {
         this.age = age;
     }
+
     public int getAge() {
         return age;
     }
+
     public void setPhone(long phone) {
         this.phone = phone;
     }
-    public long getPhone(){
+
+    public long getPhone() {
         return phone;
     }
 }
