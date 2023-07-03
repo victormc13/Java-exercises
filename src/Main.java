a9ad5f528dcc275b6ac9386ceefd9f7d7fda364d
victormc13 <viktorsb14@gmail.com>
2023-07-02 21:00:23 -0500

    Add Person class that inheritance to Client and set its values

--- src/Main.java
+++ src/Main.java
@@ -3,44 +3,21 @@
 public class Main {
 
     public static void main(String[] args) {
-        Person person = new Person();
-
-        person.setName("Victor");
-        person.setAge(23);
-        person.setPhone(931928801);
-
-        System.out.println("My name is " + person.getName());
-        System.out.println("I'm " + person.getAge() + " years old");
-        System.out.println("My phone is " + person.getPhone());
+        Client client = new Client();
+        client.name = "Victor";
+        client.age = 23;
+        client.phone = 931928801;
+        client.credit = 1312.99;
+        System.out.println("My name is " + client.name + ". I'm " + client.age + " years old " + "with phone " + client.phone + " and my currently credit is " + client.credit + "$");
     }
 }
 
 class Person {
-    private String name;
-    private int age;
-    private long phone;
+    String name;
+    int age;
+    long phone;
+}
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setPhone(long phone) {
-        this.phone = phone;
-    }
-
-    public long getPhone() {
-        return phone;
-    }
+class Client extends Person {
+    double credit;
 }
